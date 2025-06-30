e6bea4e1c94cfd1305e8f0384574362ea02c12b3
Charu Wadhwa <wadhwa.charu01@gmail.com>
2025-06-30 21:15:40 +0530

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("URL Shortener Backend Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
